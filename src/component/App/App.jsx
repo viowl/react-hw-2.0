f4b9fd3d57b9a0a7c4faab5a9f8279b17c56e1ba
viowl <sovaworkbox@gmail.com>
2024-03-06 20:27:13 +0100

    add button

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import css from "./App.module.css";
+
 function App() {
-  {
-    console.log(12);
-  }
+  const [clicks, setClicks] = useState(0);
+  const [date, setDate] = useState(Date.now());
+
+  useEffect(() => {
+    console.log("Effects are awesome!", clicks);
+  }, [clicks]);
+
+  return (
+    <div className={css.list}>
+      <button className={css.button} onClick={() => setClicks(clicks + 1)}>
+        Clicks: {clicks}
+      </button>
+      <button className={css.button} onClick={() => setDate(Date.now())}>
+        Date: {date}
+      </button>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../src/component/App/App.jsx";
 import "./index.css";
+import "modern-normalize";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
